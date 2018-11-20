97fab806878b8d94c9cb283d1c485bf450b60eb6
carlosmarinho <carluizfla@hotmail.com>
2018-11-20 00:09:01 -0200

    ajustes adsense

--- src/components/modules/google-ads.js
+++ src/components/modules/google-ads.js
@@ -24,26 +24,14 @@ class GoogleAds extends Component {
                         data-ad-client="ca-pub-7471205086445538"
                         data-ad-slot="4780433715"></ins>
                 </div>
-                <div className="adsense-tablet">
-                    {/*<!-- soumaisniteroi horizontal 468x60 -->*/}
-                    <ins className="adsbygoogle"
-                        style={{display:'inline-block',width:'468px',height:'60px'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="8029413581"></ins>
-                </div>
                 <div className="adsense-mobile">
-                    {/*<!-- soumaisniteroi banner mobile 320 x 100 -->*/}
+                    {/*<!-- Soumaisniteroi Responsivo -->*/}
                     <ins className="adsbygoogle"
-                        style={{display:'inline-block',width:'320px',height:'100px'}}
+                        style={{display:'block'}}
                         data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="1899404918"></ins>
-                </div>
-                <div className="adsense-small-square">
-                    {/*<!-- Soumaisniteroi quadrado 200x200 -->*/}
-                    <ins className="adsbygoogle"
-                        style={{display:'inline-block',width:'200px',height:'200px'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="4917284641"></ins>
+                        data-ad-slot="6155921103"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
                 </div>
             </div>
         )
