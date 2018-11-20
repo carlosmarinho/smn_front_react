e7d79bf7252575afabc6a99aa5da4d95ee0d3b7f
carlosmarinho <carluizfla@hotmail.com>
2018-11-20 17:32:13 -0200

    ajuste nas noticias compartilhar no...

--- src/components/modules/page-item.js
+++ src/components/modules/page-item.js
@@ -80,9 +80,9 @@ class PageItem extends Component {
                     <span>{this.datePtBr(new Date(item.createdAt))}</span>
                     <div className="share-btn share-pad-bot ">
                         <ul>
-                            <li><Link to={'/'}><i className="fa fa-facebook fb1"></i> Share On Facebook</Link> </li>
-                            <li><Link to={'/'}><i className="fa fa-twitter tw1"></i> Share On Twitter</Link> </li>
-                            <li><Link to={'/'}><i className="fa fa-google-plus gp1"></i> Share On Google Plus</Link> </li>
+                            <li><Link to={'/'}><i className="fa fa-facebook fb1"></i>Compartilhar <span>no Facebook</span></Link> </li>
+                            <li><Link to={'/'}><i className="fa fa-twitter tw1"></i>Compartilhar <span>no Twitter</span></Link> </li>
+                            <li><Link to={'/'}><i className="fa fa-google-plus gp1"></i>Compartilhar <span>no Google Plus</span></Link> </li>
                         </ul>
                     </div>
                     <div dangerouslySetInnerHTML={{__html: (item)?item.descricao:'Carregando ...'}} ></div>
