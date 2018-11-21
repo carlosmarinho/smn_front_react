b92e06b252fadd193d85bbf0c8532edd696fe9b6
carlosmarinho <carlos.marinho.contratada@lasa.com.br>
2018-11-21 18:51:50 -0200

    modificado o google ads q não esta funcionando

--- src/components/modules/google-ads.js
+++ src/components/modules/google-ads.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
+import windowSize from 'react-window-size';
+
 
 class GoogleAds extends Component {
 
-    componentDidMount () {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    constructor(){
+        super();
+
+        this.state = {
+                        width: 0,
+                        teste: 10,
+                     }
+
+        this.getAdsenseByWidth = this.getAdsenseByWidth.bind(this);
     }
 
-    render(){
-       
-        return(
-            <div className="text-center">
+    componentDidMount () {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
 
+        //this.setState({windowWidth: this.props.windowWidth});
+        
+    }
 
-                <div className="adsense-desktop">                    
-                    {/*<!-- soumaisniteroi Cabeçalho 728x90 -->*/}
-                    <ins className="adsbygoogle"
-                        style={{display:'inline-block',width:'728px',height:'90px'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="4780433715"></ins>
-                </div>
-                
-                <div className="adsense-mobile">
+    componentWillReceiveProps(nextProps) {
+        /* if(nextProps && nextProps.windowWidth)
+            this.setState({windowWidth: nextProps.windowWidth}); */
+    }
+
+    getAdsenseByWidth(width=0){
+        //console.log("iidth: ", this.state.teste)
+        if(width <= 450){
+            return(
+                <div className="adsense-mobile">adsense-mobile
                     {/*<!-- Soumaisniteroi Responsivo -->*/}
                     <ins className="adsbygoogle"
                         style={{display:'block'}}
@@ -29,10 +40,53 @@ class GoogleAds extends Component {
                         data-ad-format="auto"
                         data-full-width-responsive="true"></ins>
                 </div>
+            )
+        }
+        else if(width <= 727){
+            return(
+                <div className="adsense-tablet">adsense-tablllllet
+                    {/*<!-- soumaisniteroi horizontal 468x60 -->*/}
+                    <ins className="adsbygoogle"
+                        style={{display:'inline-block',width:'468px',height:'60px'}}
+                        data-ad-client="ca-pub-7471205086445538"
+                        data-ad-slot="8029413581"></ins>
+                </div>
+            )
+        }
+        else if(width < 992){
+            return(
+                <div className="adsense-desktop">adsense-deskkkkktoopp                    
+                    {/*<!-- soumaisniteroi Cabeçalho 728x90 -->*/}
+                    <ins className="adsbygoogle"
+                        style={{display:'inline-block',width:'728px',height:'90px'}}
+                        data-ad-client="ca-pub-7471205086445538"
+                        data-ad-slot="4780433715"></ins>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div className="adsense-desktop-big">adsense-desktoppp-biiiiig                    
+                    {/*<!-- Soumaisniteroi banner grande 970x90 -->*/}
+                    <ins className="adsbygoogle"
+                        style={{display:'inline-block',width:'970px',height:'90px'}}
+                        data-ad-client="ca-pub-7471205086445538"
+                        data-ad-slot="7942069085"></ins>
+                </div>
+            )
+        }
+
+    }
+
+    render(){
+        //console.log("window width: ", this.state.windowWidth)
+        return(
+            <div className="text-center">
+                {this.getAdsenseByWidth()}
             </div>
         )
     }
 
 }
 
-export default GoogleAds;
+export default windowSize(GoogleAds)
