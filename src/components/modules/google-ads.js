875e6a9d18cbf26fac6af06872b0d313884e4945
carlosmarinho <carlos.marinho.contratada@lasa.com.br>
2018-11-22 12:37:28 -0200

    alterações do adsense

--- src/components/modules/google-ads.js
+++ src/components/modules/google-ads.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import windowSize from 'react-window-size';
+import Adsense from 'react-adsense';
 
 
 class GoogleAds extends Component {
@@ -8,18 +9,28 @@ class GoogleAds extends Component {
         super()
 
         this.state = {
-            myWidth: 500
+            myWidth: 0
         }
     }
 
     componentDidMount () {
         
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        
 
         console.log("my width props: ", this.props.windowWidth);
         this.setState({
             myWidth: this.props.windowWidth
         })
+        
+        //if(this.state.myWidth != 0)
+    }
+    
+    componentWillReceiveProps(nextProps){
+        if(nextProps && nextProps.windowWidth){
+            
+            
+            console.log("vou setar o state aqui");
+        }
     }
 
     getAdsenseByWidth(width=0){
@@ -28,12 +39,13 @@ class GoogleAds extends Component {
             return(
                 <div className="adsense-mobile">
                     {/*<!-- Soumaisniteroi Responsivo -->*/}
-                    <ins className="adsbygoogle"
-                        style={{display:'block'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="6155921103"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
+                    <Adsense.Google
+                        client='ca-pub-7471205086445538'
+                        slot='6155921103'
+                        style={{ display: 'block' }}
+                        format='auto'
+                        responsive='true'
+                    />
                 </div>
             )
         }
@@ -41,10 +53,12 @@ class GoogleAds extends Component {
             return(
                 <div className="adsense-tablet">
                     {/*<!-- soumaisniteroi horizontal 468x60 -->*/}
-                    <ins className="adsbygoogle"
+                    <Adsense.Google
+                        client='ca-pub-7471205086445538'
+                        slot='8029413581'
                         style={{display:'inline-block',width:'468px',height:'60px'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="8029413581"></ins>
+                        format=''
+                    />
                 </div>
             )
         }
@@ -52,10 +66,12 @@ class GoogleAds extends Component {
             return(
                 <div className="adsense-desktop">                   
                     {/*<!-- soumaisniteroi Cabeçalho 728x90 -->*/}
-                    <ins className="adsbygoogle"
+                    <Adsense.Google
+                        client='ca-pub-7471205086445538'
+                        slot='4780433715'
                         style={{display:'inline-block',width:'728px',height:'90px'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="4780433715"></ins>
+                        format=''
+                    />
                 </div>
             )
         }
@@ -63,10 +79,12 @@ class GoogleAds extends Component {
             return(
                 <div className="adsense-desktop-big">              
                     {/*<!-- Soumaisniteroi banner grande 970x90 -->*/}
-                    <ins className="adsbygoogle"
+                    <Adsense.Google
+                        client='ca-pub-7471205086445538'
+                        slot='7942069085'
                         style={{display:'inline-block',width:'970px',height:'90px'}}
-                        data-ad-client="ca-pub-7471205086445538"
-                        data-ad-slot="7942069085"></ins>
+                        format=''
+                    />
                 </div>
             )
         }
@@ -75,21 +93,22 @@ class GoogleAds extends Component {
 
     render(){
         let width = this.state.myWidth;
-        if(width === 500){
+        console.log("minha width no render: ", width)
+        if(width === 0){
+            console.log("width no null: ", width);
             return null;
 
-            console.log("width no null: ", width);
         }
         else{
-            return <div>minha divdididididiididid</div>
-        }
 
-        /* console.log("width: ", width);
-        return(
-            <div className="text-center">
-                {this.getAdsenseByWidth(width)}
-            </div>
-        ) */
+            console.log("width: ", width);
+            return(
+                <div className="text-center">
+                    {this.getAdsenseByWidth(width)}
+                    
+                </div>
+            ) 
+        }
     }
 
 }
