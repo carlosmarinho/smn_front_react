c8300c0c1a27f139806dccebbfe087fe045aa96a
Admin soumaisniteroi.com.br <admin@soumaisniteroi.com.br>
2018-12-30 15:47:56 -0200

    changed server.js

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(__dirname, './build')));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', function (req, res) {
+
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  res.sendFile(filePath);
+
 });
 
-app.listen(process.env.PORT || 8080);
+console.log("Server loading on port: ", process.env.PORT || 21040)
+app.listen(process.env.PORT || 21040);
