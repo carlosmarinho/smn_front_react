82f831a463b3e5c82416f070f3a571ba27e37467
carlosmarinho <carluizfla@hotmail.com>
2018-12-30 21:56:14 -0200

    voltado com o server sem o meta-tag

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const path = require('path');
 const app = express();
 const fs = require('fs')
 
-const MetaTagsServer = require('react-meta-tags/server');
-const MetaTagsContext = require('react-meta-tags');
-
-//app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 
 app.get('/ping', function (req, res) {
@@ -24,24 +21,9 @@ app.get('/*', function (req, res) {
   console.log('Home page visited!');
   const filePath = path.resolve(__dirname, './build', 'index.html');
 
-  // read in the index.html file
-  fs.readFile(filePath, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    
-    // replace the special strings with server generated strings
-    data = data.replace(/\$OG_TITLE/g, 'Home Page');
-    data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
-    result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-
-    console.log(result);
-    res.send(result);
-  });
+  
 
 }); 
-
-app.use(express.static(path.resolve(__dirname, './build')));
 
 
 console.log("Server loading on port: ", process.env.PORT || 21041)
